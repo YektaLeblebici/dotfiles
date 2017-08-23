62d52aae7965aaf0216f9c893d44d0ae6efffc0e
Yekta Leblebici <yekta@iamyekta.com>
2017-08-23 23:11:01 +0300

    additional dock and screensaver defaults

--- defaults.sh
+++ defaults.sh
@@ -9,10 +9,18 @@ defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -float 0.2
 defaults write com.apple.dock magnification -bool true
+defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock orientation bottom
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Keyboard settings
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Screensaver
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 300
 # Safari
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
