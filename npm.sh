947319557d1ed5d7310b10bfa637a43a274030eb
Yekta Leblebici <yekta@iamyekta.com>
2023-04-13 19:02:38 +0000

    update npm packages

--- npm.sh
+++ npm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PACKAGES=( serverless markdownlint markdownlint-cli )
+PACKAGES=( serverless markdownlint markdownlint-cli yaml-language-server jsonschema2md )
 
 npm install -g "${PACKAGES[@]}"
 
